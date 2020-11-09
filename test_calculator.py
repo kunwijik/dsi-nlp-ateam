28f5d15843897df74c439051cd840ac83e0f29a0
Eli Kasai <kunwijik@gmail.com>
2020-11-09 12:48:49 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,14 @@
 """
-Unit tests for the calculator library
+Unit tests for the calculator library.
 """
 
 import calculator
 
 
-class TestCalculator:
-    
+class TestCalculaor:
+
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
-        
+
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
